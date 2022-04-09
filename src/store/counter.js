84bc5d3fc47b6f86c6cda0e7be92756231ab6db8
liuming <15926724726@163.com>
2022-04-09 11:59:38 +0800

    初始化counter.js

---
+++ src/store/counter.js
@@ -0,0 +1,17 @@
+import { makeAutoObservable } from "mobx";
+
+// first mobx demo
+class CounterStore {
+	// 1.定义数据
+	count = 0;
+	constructor() {
+		// 2.把数据弄成响应式
+		makeAutoObservable(this);
+	}
+	// 3.定义action函数（修改数据）
+	addCount() {
+		this.count++;
+	}
+}
+
+// 4.实例化 然后导出给react使用
