2ded91f3f20119ef5e33386c4779eaa9cb23e82b
liuming <15926724726@163.com>
2022-04-10 13:00:46 +0800

    嵌套路由

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import { Routes, Route } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import Login from "./Login";
+import Layout from "./Layout";
+import Board from "./Board";
+import Article from "./Article";
+import NotFound from "./NotFound";
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Link to={"/"}>首页</Link>
-				<Link to={"/about"}>关于</Link>
 				<Routes>
-					<Route path="/:id" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
+					<Route path="/" element={<Layout />}>
+						{/* <Route path="board" element={<Board />} /> */}
+						{/* 默认二级 添加index */}
+						<Route index element={<Board />} />
+						<Route path="article" element={<Article />} />
+					</Route>
+					<Route path="/login" element={<Login />} />
+					{/* 当所有的路径都没有匹配时，返回NotFound */}
+					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
